bf77b3730fa01b17b5b9216c7824dceb22d5e896
srijitmessi <30919524+srijitmessi@users.noreply.github.com>
2019-05-10 06:46:14 +0530

    Add files via upload

---
+++ Homepage/home.php
@@ -0,0 +1,241 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+        <title>VT-PMS Home</title>
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <!-- Footer -->
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <!-- Custom Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+        <!-- Plugin CSS -->
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/creative.css" type="text/css">
+          <link href='https://fonts.googleapis.com/css?family=Carter One' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Chicle' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Aladin' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family= Arima Madurai' rel='stylesheet'>
+        
+    </head>
+    
+    <body id="page-top">
+        
+        <style>
+            nav.navbar
+            {
+                color: white;
+                height: 70px; background-color:#2E4053  ;
+                font-family:Carter One;
+                letter-spacing: 2px;
+
+            }
+             a.navbar-brand
+             {
+                font-size: 2em;
+                color: #eb3812;
+                margin-top: 10px;
+             }
+              a.navbar-brand:hover
+              {
+                letter-spacing: 3px;
+              }
+        </style>
+        <nav id="mainNav" class="navbar navbar-fixed-top">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header" >
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#" >Veltech Placement Portal</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <style>
+                        ul.nav li
+                        {
+                       
+                            color: white;
+                            margin-top: 10px;
+                        }
+                        li a.page-scroll:hover
+                        {
+                            
+                            padding-top: 10px;
+                            padding-bottom: 25px;
+
+                        }
+
+                    </style>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a class="page-scroll" href="../Profilers/SProfile/index.php">Student Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/HODProfile/index.php">HOD Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/PProfile/index.php">Placement Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/PriProfile/index.php">Administrative Login</a>
+                        </li>
+                    </ul>
+                
+            </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+        <style>
+            .heading
+            {
+                height: 70px;
+            }
+        </style>
+        <div class="heading">
+            
+        </div>
+        <div class="section01">
+           <div class="container">
+           
+               <div class="row">
+                   <div class="col-lg-5"></div>
+                   <div class="col-lg-7" style="background-color: ">
+                     <div class="well">
+                        <div class="header">
+                <div class="header-content-inner">
+                    <h1>PLACEMENT MANAGEMENT SYSTEM</h1>
+                    <hr />
+                    <p>We are here to Build your Skills and Career with our Driven Passion and Reality.
+                       Click Below to Get Our Current Drive Details</p>
+                        <div class="row">
+                            <div class="col-lg-4"></div>
+                             <div class="col-lg-4" style="background-color:">
+                                 <a href="../Drives/index.php" class="btn btn-primary btn-xl page-scroll">Current Drives</a>
+                             </div>
+                             <div class="col-lg-4" style="background-color:">
+                                 <a href="../Training/index.php" class="btn btn-primary btn-xl page-scroll">Training</a>
+                             </div>
+                              <div class="col-lg-4"></div>
+                        </div>
+                    
+                </div>
+            </div>
+    
+                   </div>
+                  
+               </div>
+               </div>
+           </div>
+           
+        </div>
+        <style>
+        .section01 .header
+        {
+            
+        } 
+         .section01 .well
+         {
+            background-color: transparent;
+            border:none;
+
+         }
+        hr
+        {
+            margin-right: 280px;
+            
+        }
+        .btn-primary
+        {
+           
+           
+        }
+         .section01 .header h1
+         {
+            font-family: Aladin;
+            letter-spacing: 3px;
+            
+            text-align:right;
+         }
+           .section01 .header p
+           {
+            text-align:center;
+           }
+            .section01 
+            {
+                background-image: url(images/4.png);
+                background-repeat: no-repeat;
+                height: 100%;
+                
+                background-size: cover;
+                background-color: red;
+
+            }
+        </style>
+       
+        <div class="footer">
+			<div class="container">
+				<div class="col-md-3 ftr_navi ftr">
+					<h3>NAVIGATION</h3>
+					<ul>
+						<li>
+							<a href="../Homepage/index.php">Home</a>
+						</li>
+						<li>
+							<a href="../Profilers/SProfile/index.php">Student Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/PProfile/index.php">Placement Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/HODProfile/index.php">HOD Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/Priprofile/index.php">Administrative Login</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-3 ftr_navi ftr">
+					 
+				</div>
+				<div class="col-md-3 get_in_touch ftr">
+					<h3>GET IN TOUCH</h3>
+					<p>Veltech University</p>
+					<p>Avadi, Chennai, Indi1a</p>
+					<p>03658-25478/558585</p>
+					<a href="mailto:veltech@gmail.com">veltech@gmail.com</a>
+				</div>
+				<div class="col-md-3 ftr-logo">
+					<p>Copyright &copy; 2019 VT-PMS | Developed by
+              <a href="#" target="_parent">Veltech Synergy Technologies</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+    </body>
+<style>
+    .footer
+    {
+        background-color:#2E4053 ;
+        padding: 0px; 
+    }
+</style>
+</html>
